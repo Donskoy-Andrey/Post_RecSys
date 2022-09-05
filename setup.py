5ac44b8229e4cd102e17726c14e2c85ed2fbd831
Andrew Donskow <donskoi.com@gmail.com>
2022-09-05 15:37:04 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='The project is a recommendation system for text posts based on users data, '
-                'publications data and interactions between them.',
+    description='The project is a recommendation system for text posts based on users data publications data and interactions between them.',
     author='Donskow Andrew',
     license='',
 )
