29e1f1dae6d1480567032218ade2af05f86058c3
Andrew Donskow <donskoi.com@gmail.com>
2022-09-05 20:40:57 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ from setuptools import find_packages, setup
 setup(
     name='src',
     version='0.1.0',
-    packages=['src'],
+    author='Donskow Andrew',
+    author_email='donskow.com@gmail.com',
+    packages=find_packages(),
     description='Recommender system for text posts based on users data, \
-    publications data and interactions between them.',
-    author='Donskow Andrew'
+    publications data and interactions between them.'
 )
